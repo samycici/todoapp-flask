33c75e8033c2ede6815f8bdff9cd5f4461875cfa
Samanta Cecília de Barros Souza <samanta@MAC7448.local>
2016-04-29 15:52:18 -0300

    Adding integration tests

--- todoapp/apps.py
+++ todoapp/apps.py
@@ -11,19 +11,27 @@ db = SQLAlchemy(app)
 
 from models import *
 
+def create_new_todo():
+    todo = Todo(request.form['title'], request.form['text'])
+    db.session.add(todo)
+    db.session.commit()
+
+def edit_todo(todo_item, todo_id):
+    todo_item.title = request.form['title']
+    todo_item.text = request.form['text']
+    todo_item.done = ('done.%d' % todo_id) in request.form
+    db.session.commit()
+
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html',
         todos = Todo.query.order_by(Todo.pub_date.desc()).all()
     )
 
-
 @app.route('/new/', methods=['GET', 'POST'])
 def new():
     if request.method == 'POST':
-        todo = Todo(request.form['title'], request.form['text'])
-        db.session.add(todo)
-        db.session.commit()
+        create_new_todo()
         return redirect(url_for('index'))
     return render_template('new.html')
 
@@ -32,10 +40,7 @@ def show_or_update(todo_id):
     todo_item = Todo.query.get(todo_id)
     if request.method == 'GET':
         return render_template('view.html', todo = todo_item)
-    todo_item.title = request.form['title']
-    todo_item.text = request.form['text']
-    todo_item.done = ('done.%d' % todo_id) in request.form
-    db.session.commit()
+    edit_todo(todo_item, todo_id)
     return redirect(url_for('index'))
 
 @app.errorhandler(404)
--- todoapp/tests/unit_tests.py
+++ todoapp/tests/unit_tests.py
@@ -1,18 +1,35 @@
-from todoapp.apps import app,db
+from todoapp.apps import app
 import unittest
-from todoapp.models import Todo
+from mock import patch
+
+class ConfigUnitTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.app = app
+
+    def test_dev_config(self):
+        app.config.from_object('config.DevelopmentConfig')
+        assert self.app.config['DEBUG'] is True
+        assert self.app.config['DEVELOPMENT'] is True
+        assert self.app.config['TESTING'] is True
+        assert self.app.config['CSRF_ENABLED'] is False
+        self.assertIn('sqlite:///',self.app.config['SQLALCHEMY_DATABASE_URI'])
+        self.assertIn('todo.db',self.app.config['SQLALCHEMY_DATABASE_URI'])
+
+    def test_stg_config(self):
+        app.config.from_object('config.StagingConfig')
+        assert self.app.config['DEBUG'] is True
+        assert self.app.config['DEVELOPMENT'] is True
+        assert self.app.config['TESTING'] is False
+        assert self.app.config['CSRF_ENABLED'] is True
+        self.assertIn('postgresql://admin:admin@postgres/todo',self.app.config['SQLALCHEMY_DATABASE_URI'])
 
 class ToDoUnitTests(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
         self.app = app.test_client()
-        db.create_all()
-
-    @classmethod
-    def tearDownClass(self):
-        db.session.remove()
-        db.drop_all()
 
     def test_pagenotfound_statuscode(self):
         result = self.app.get('/missing-page')
@@ -23,30 +40,6 @@ class ToDoUnitTests(unittest.TestCase):
         result = self.app.get('/missing-page')
 
         self.assertIn('Page Not Found', result.data)
-
-    def test_home_empty(self):
-        rv = self.app.get('/')
-        self.assertEqual(rv.status_code, 200)
-        assert 'All Items' in rv.data
-
-    def test_new_todo(self):
-        self.app.post('/new/', data=dict(title='Unit Test 2', text= 'Unit Test Description 2'))
-        rv = self.app.get('/')
-        assert 'Unit Test 2' in rv.data
-        assert 'Unit Test Description 2' in rv.data
-
-    def test_show_todo(self):
-        rv = self.app.get('/todos/1')
-        assert 'Unit Test 2' in rv.data
-        assert 'Unit Test Description 2' in rv.data
-
-    def test_update_todo(self):
-        self.app.post('/todos/1', data=dict(title='Unit Test Updated', text= 'Unit Test Description Updated'))
-        rv = self.app.get('/')
-        assert 'Unit Test 2' not in rv.data
-        assert 'Unit Test Description 2' not in rv.data
-        assert 'Unit Test Updated' in rv.data
-        assert 'Unit Test Description Updated' in rv.data
 
 
 if __name__ == "__main__":
