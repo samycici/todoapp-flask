54f2253df68f56fc6e08100788086cdad67fc4bb
Samanta Cecília de Barros Souza <samanta@MAC7448.local>
2016-04-29 18:31:48 -0300

    Fix address

--- start.sh
+++ start.sh
@@ -3,5 +3,4 @@ cd todoapp-flask
 python manage.py db init
 python manage.py db migrate
 python manage.py db upgrade
-nohup python todoapp/apps.py &
-nohup python todoapp/api.py &
+python todoapp/apps.py & python todoapp/api.py
