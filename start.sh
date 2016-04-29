fba80f8ca4febc19a6427035e468c335748f4171
Samanta Cecília de Barros Souza <samanta@MAC7448.local>
2016-04-29 18:15:21 -0300

    Creating API

--- start.sh
+++ start.sh
@@ -3,4 +3,5 @@ cd todoapp-flask
 python manage.py db init
 python manage.py db migrate
 python manage.py db upgrade
-python todoapp/apps.py
+nohup python todoapp/apps.py &
+nohup python todoapp/api.py &
